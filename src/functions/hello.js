c52966a435694390e9efde42149b652bcad0e847
liwenmarcin <marcin.liwen@sohoshop.pl>
2021-09-02 14:00:54 +0200

    function hello

--- src/functions/hello.js
+++ src/functions/hello.js
@@ -1,6 +1,6 @@
-exports.handler = async function(event, context) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({message: "Hello World"})
-    };
-}
+exports.handler = function (event, context, callback) {
+    callback(null, {
+      statusCode: 200,
+      body: "Hello, World",
+    });
+  };
